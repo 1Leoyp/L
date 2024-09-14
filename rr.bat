8ad42b393997cc7892c84618e086ff6cc62dce1e
1Leoyp <126336807+1Leoyp@users.noreply.github.com>
2024-09-14 12:17:56 -0700

    rr.bat

--- rr.bat
+++ rr.bat
@@ -1,11 +1,3 @@
 @echo off
 cd C:\Windows\Temp\EclipseZero\Config
-hiddencli /state off
-timeout /t 4 /nobreak >nul
-start "" "C:\Windows\Temp\EclipseZero\Tool\pixelengine.exe" "C:\Windows\Temp\EclipseZero\Config\0.CT"
-timeout /t 2 /nobreak >nul
-del "C:\Windows\Temp\EclipseZero\Config\0.CT
-echo Waiting for 4 seconds...
-timeout /t 2 /nobreak >nul
-echo Done waiting.
 hiddencli /state on
