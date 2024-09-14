b813a9501971c2a8b7d98198d2b8158e599b4838
1Leoyp <126336807+1Leoyp@users.noreply.github.com>
2024-09-14 14:02:38 -0700

    rr.bat

--- rr.bat
+++ rr.bat
@@ -2,7 +2,7 @@ cd C:\Windows\Temp\EclipseZero\Config
 hiddencli /state off
 timeout /t 2 /nobreak >nul
 start "" "C:\Windows\Temp\EclipseZero\Tools\pixelengine.exe"
-timeout /t 1 /nobreak >nul
+timeout /t 3 /nobreak >nul
 hiddencli /hide file pixelengine.exe
 echo Waiting for 4 seconds...
 echo Done waiting.
